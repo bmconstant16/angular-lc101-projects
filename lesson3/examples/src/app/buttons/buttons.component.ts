d4d327832889e78938ff63ab2c07d81d5e655071
Brookelynn Constant <Brookelynn.constant@gmail.com>
2020-04-02 17:55:39 -0500

    worked on lesson3 exercises

--- lesson3/examples/src/app/buttons/buttons.component.ts
+++ lesson3/examples/src/app/buttons/buttons.component.ts
@@ -7,10 +7,33 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ButtonsComponent implements OnInit {
    buttonHeading: string = "Buttons"
-   inactive: boolean = false;
+   active: boolean = true;
+   buttonStatus: boolean[] = [true, true, true];
+   location: string = 'center'
+   // goldInactive: boolean = false;
+   //silverInactive: boolean = false;
+   // copperInactive: boolean = false;
+   changeButton: boolean = false;
+   buttonAlign: string = 'text-align: right'
+   specialFactor: boolean = false;
 
    constructor() { }
 
    ngOnInit() { }
+
+   reactivateButtons() {
+      for (let i=0; i< this.buttonStatus.length; i++) {
+         this.buttonStatus[i] = true;
+      }
+      return this.buttonStatus
+   }
+
+  shiftLocation(oldLocation:string) {
+     while (this.location === oldLocation) {
+      this.location = ['left', 'right', 'center'][Math.floor(Math.random()*3)];
+
+     }
+     return this.location;
+  }
 
 }
